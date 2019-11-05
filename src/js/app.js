048a956f56bfa392f6b2b6497ba02182b59aaa9e
taydelver <51969548+taydelver@users.noreply.github.com>
2019-11-05 12:41:16 -0600

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// Import module here. Give it whatever name you want.
+
+import SingleModule from './modules/single'
+import { FunctionOne, FunctionTwo } from './modules/multiple'
+import Another from './modules/another'
+
+
+// Runn all the imported functions
+
+SingleModule.myFunction()
+FunctionOne()
+FunctionTwo()
+Another.one()
+Another.two()
